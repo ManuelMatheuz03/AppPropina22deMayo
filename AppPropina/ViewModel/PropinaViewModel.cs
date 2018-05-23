c2c22f7f8bc058c315914c16d95394a941f18764
ManuelMatheuz03 <38057638+ManuelMatheuz03@users.noreply.github.com>
2018-05-22 19:54:38 -0500

    Initial commit

---
+++ AppPropina/ViewModel/PropinaViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPropina.ViewModel
+{
+
+    using Model;
+    using Xamarin.Forms;
+
+    class PropinaViewModel
+    {
+        #region Propiedades
+
+        public Propina PropinaModel { get; set; }
+        public Command CalcularCommand { get; set; }
+
+        #endregion
+
+        #region Constructores
+
+        public PropinaViewModel()
+        {
+            PropinaModel = new Propina();
+            CalcularCommand = new Command(Calcular);
+        }
+
+        private void Calcular(object obj) {
+
+            PropinaModel.TotalCuenta = decimal.Parse(entTotal.Text);
+            PropinaModel.LaPropina = int.Parse(entPropina.Text);
+            PropinaModel.NumeroPersonas = int.Parse(entPersonas.Text);
+
+            var totalPropina = (total * (propina / 100)).ToString();
+
+            txtPropina.Detail = totalPropina.ToString();
+            txtPersona.Detail = (total / personas).ToString();
+            txtTotal.Detail = (total + totalPropina).ToString();
+
+        }
+
+        #endregion
+
+
+    }
+}
